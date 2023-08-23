6c4516f589b1fa55b2498c07202e80a2c80eb399
alexnurin <an362@mail.ru>
2023-08-23 15:57:07 +0200

    Created users-app and custom User-model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name='email',
+        unique=True,
+        max_length=255
+    )
+    first_name = models.CharField(
+        verbose_name='name',
+        max_length=100
+    )
+    last_name = models.CharField(
+        verbose_name='surname',
+        max_length=100
+    )
+    is_staff = models.BooleanField(
+        verbose_name='admin',
+        default=False
+    )
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
