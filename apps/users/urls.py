887df51553aec633e93586b7f9b183401a44497a
alexnurin <an362@mail.ru>
2023-09-08 18:54:45 +0300

    working vk login MVP

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("users", views.users, name="users"),
-    path("register/", views.register, name="register"),
+    path("register", views.register, name="register"),
 ]
