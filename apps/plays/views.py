9d2f71e7345c8129ed798853428fbd64fac8a3a2
alexnurin <an362@mail.ru>
2023-09-21 00:52:18 +0300

    creating records

--- apps/plays/views.py
+++ apps/plays/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Play
-from .forms import PlayForm
+from .models import Play, Record
+from .forms import PlayForm, RecordForm
 
 
 def plays(request):
@@ -35,7 +35,34 @@ def play(request):
     if this_play:
         this_play = this_play[0]
     else:
-        this_play = "empty"
+        return redirect("plays")
 
     context = {"play_id": play_id, "this_play": this_play}
     return render(request, "plays/play_site.html", context)
+
+
+def records(request):
+    records_list = Record.objects.order_by("id").reverse()
+    return render(
+        request,
+        "plays/records.html",
+        {"title": "Все записи", "records": records_list},
+    )
+
+
+def create_record(request):
+    error = ""
+    if request.method == "POST":
+        form = RecordForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("records")
+        else:
+            error = "Форма была неверной"
+    form = RecordForm()
+    context = {
+        "form": form,
+        "error": error,
+    }
+
+    return render(request, "plays/create_record.html", context)
