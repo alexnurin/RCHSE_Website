d68c048cfb4b30e88b10a6e7eb887f83d1d0f3b9
alexnurin <an362@mail.ru>
2024-03-23 19:32:09 +0300

    added robots.txt

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
+from django.http import HttpResponse
 
+@require_GET
+def robots_txt(request):
+    lines = [
+        "User-Agent: *",
+        "Disallow: /private/",
+        "Disallow: /junk/",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
 
 def index(request):
     return render(request, "main/main.html", {"title": "Главная страница сайта"})
